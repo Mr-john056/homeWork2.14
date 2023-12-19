297f2673a0cd6dc3e2b50e5be04d74a5c5366626
Mr-john056 <ekulakov056@gmail.com>
2023-12-19 13:32:11 +0500

    create repostitory and doing base algorithm

---
+++ src/main/java/sky/pro/hw24/StringListImpl.java
@@ -0,0 +1,96 @@
+package sky.pro.hw24;
+
+public class StringListImpl implements StringList {
+
+    private final String[] storage;
+    private int size;
+
+    public StringListImpl(){
+        storage = new String[10];
+    }
+
+    public StringListImpl(int initSize){
+        storage = new String[initSize];
+    }
+
+    @Override
+    public String add(String item) {
+        return null;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {return false;}
+
+    @Override
+    public int size(){return 0;}
+
+    @Override
+    public boolean isEmpty(){return false;}
+
+    @Override
+    public void clear(){}
+
+    @Override
+    public String[] toArray(){return new String[0];}
+
+    private void validateItem(String item){
+        if (item == null) {
+            throw new NullItemException();
+        }
+    }
+
+    private void validateSize(){
+        if(size == storage.length){
+            throw new StorageIsFullException();
+        }
+    }
+
+    private void validateIndex(int index){
+        if(index < 0 || index > size){
+            throw new invalidException();
+        }
+    }
+
+
+
+}
